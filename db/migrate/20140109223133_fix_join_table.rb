5240868d64e48f9a771336cbc97b18d805b9e887
Nikki Thean <nikki.thean@gmail.com>
2014-01-09 22:00:25 -0500

    Remove bad migrations

--- db/migrate/20140109223133_fix_join_table.rb
+++
@@ -1,7 +0,0 @@
-class FixJoinTable < ActiveRecord::Migration
-  def change
-    drop_table :acts_amoebas
-    create_join_table :amoebas, :acts do |t|
-    end
-  end
-end
--- db/migrate/20140109223504_fix_amoeba_acts_table.rb
+++ db/migrate/20140109223504_fix_amoeba_acts_table.rb
@@ -1,7 +1,5 @@
 class FixAmoebaActsTable < ActiveRecord::Migration
   def change
-    drop_table :amoeba_acts
-    
     create_table :amoeba_acts, id: false do |t|
       t.belongs_to :amoeba
       t.belongs_to :act
